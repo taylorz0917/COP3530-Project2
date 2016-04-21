c9698d33317802abdb2532cd32a8d29a2f2455c7
jsgonz <j.gonzalez@ufl.edu>
2016-04-21 16:43:15 -0400

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-
+	int numrealms, nummagi, power;
+	string charm, currcharm, destcharm;
+	
+	cin >> numrealms;
+	
+	for(int i = 0; i < numrealms; i++)
+	{
+		cin >> charm;
+		cin >> nummagi;
+		
+		for(int j = 0; j < nummagi; i++)
+		{
+			cin >> power;
+		}
+	}
+	
+	cin >> currcharm;
+	cin >> destcharm;
 }
