72e7230bcb82195e01624bd3731d33fd84f7128b
taylorz0917 <taylorz0917@ufl.edu>
2016-04-26 12:58:29 -0400

    Update to pointer issues

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-
-
 class realm
 {
 public:
@@ -22,46 +20,46 @@ private:
 	int maxinc;
 };
 
-realm::realm(string charm, vector <int> magi):charm(charm),magi(magi){
-	
-	vector<int> b, temp(magi.size());
-	int s,d;
+realm::realm(string charm, vector <int> magi) :charm(charm), magi(magi) {
 
-	if(magi.empty()) return;
+	vector<int> b, temp(magi.size());
+	int s, d;
+
+	if (magi.empty()) return;
 
 	b.push_back(0);
 
-	for(unsigned int i = 0; i< magi.size(); i++)
+	for (unsigned int i = 0; i< magi.size(); i++)
 	{
-		if(magi[b.back()] < magi[i])
+		if (magi[b.back()] < magi[i])
 		{
 			temp[i] = b.back();
 			b.push_back(i);
 			continue;
 		}
-		for(s = 0, d = b.size()-1; s<d;)
+		for (s = 0, d = b.size() - 1; s<d;)
 		{
-			int c = (s + d)/2;
-			if(magi[b[c]] < magi[i])
-				s=1+c;
+			int c = (s + d) / 2;
+			if (magi[b[c]] < magi[i])
+				s = 1 + c;
 			else
 				d = c;
 		}
 		if (magi[i] < magi[b[s]])
 		{
-			if(s > 0)
-				temp[i] = b[s-1];
+			if (s > 0)
+				temp[i] = b[s - 1];
 			b[s] = i;
 		}
 	}
-	for(s = b.size(), d = b.back(); s--; d = temp[d])
-			b[s] = d;
-		
-	for(unsigned int i = 0; i< b.size(); i++)
+	for (s = b.size(), d = b.back(); s--; d = temp[d])
+		b[s] = d;
+
+	for (unsigned int i = 0; i< b.size(); i++)
 	{
 		cout << magi[b[i]] << endl;
 	}
-		this->maxinc = b.size();
+	this->maxinc = b.size();
 }
 
 struct edge
@@ -69,64 +67,73 @@ struct edge
 	realm *from;
 	realm *to;
 	int weight;
-	
-	edge(from, to, weight): from(from), to(to), weight(weight){}
+
+	edge(realm *from, realm *to, int weight) : from(from), to(to), weight(weight) {}
 };
 
 class graph
 {
 public:
 	graph(int v, vector<realm> realms);
+	void addEdge(int dist, realm *s, realm *d);
 	int dist(string u, string v);
-	void edge(int dist, realm s, realm d);
+	//void addEdge(int dist, realm s, realm d);
 private:
 	int vertices;
 	list<edge> *adj;
 	vector<realm> realms;
 };
 
-graph::graph(int v, vector<realm> realms):vertices(v),realms(realms){
+graph::graph(int v, vector<realm> realms) :vertices(v), realms(realms) {
 	this->adj = NULL;
 }
 
-void graph:edge(int dist, realm s, realm d)
+void graph::addEdge(int dist, realm *s, realm *d)
 {
-	edge e = new edge(s, d, dist);
+	edge *e = new edge(s, d, dist);
 	//cout << e->from << " " << e->to << " " << e->weight << endl; 
 }
 
-int graph::dist(string u, string v)
+int dist(string u, string v)
 {
 	int n = u.length();
 	int m = v.length();
-	
-	int matrix[n+1][m+1];
-	
-	for(int i = 0; i<= n; i++){
-		for(int j = 0; j<=m; j++){
-					
-			if(i == 0)
+
+	//int matrix[n + 1][m + 1];
+
+	int *ary = new int[n*m];
+
+	// ary[i][j] is then rewritten as
+	//ary[i*n + j];
+
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+
+			if (i == 0)
 			{
-				matrix[i][j] = j;
+				ary[i*n + j] = j;
 			}
-			else if(j ==0)
+			else if (j == 0)
 			{
-				matrix[i][j] = i;
+				ary[i*n + j] = i;
 			}
-			else if( u[i-1] == v[j-1])
+			else if (u[i - 1] == v[j - 1])
 			{
-				matrix[i][j] = matrix[i-1][j-1];
+				ary[i*n + j] = ary[(i - 1)*n + (j - 1)];
 			}
 			else
 			{
-				int min1 = min(matrix[i][j-1],matrix[i-1][j]);
-				int min2 = min(matrix[i-1][j-1],matrix[i-1][j]);
-				min1 = min(min1,min2);
-				matrix[i][j] = 1+ min1;	
+				int min1 = min(ary[i*n + (j-1)] , ary[(i-1)*n + j]);
+				int min2 = min(ary[(i-1)*n + (j-1)] , ary[(i - 1)*n + j]);
+				min1 = min(min1, min2);
+				ary[i*n + j] = 1 + min1;
 			}
 		}
 	}
-		return matrix[n][m];
+	int result = ary[n*n + m];
+	delete[] ary;
+	return result;
 }
 
 int main()
@@ -135,15 +142,17 @@ int main()
 	string charm, currcharm, destcharm;
 	vector <int> vec;
 	vector <realm*> realms;
-	
+
+	cout << dist("florida", "flower") << endl;
+	/*
 	cin >> numrealms;
 
-	for(int i = 0; i < numrealms; i++)
+	for (int i = 0; i < numrealms; i++)
 	{
 		cin >> charm;
 		cin >> nummagi;
 
-		for(int j = 0; j < nummagi; j++)
+		for (int j = 0; j < nummagi; j++)
 		{
 			cin >> power;
 			vec.push_back(power);
@@ -151,7 +160,9 @@ int main()
 		realm *r = new realm(charm, vec);
 		realms.push_back(r);
 	}
-	
+
 	cin >> currcharm;
 	cin >> destcharm;
+	*/
+	return 0;
 }
